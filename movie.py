8d68301197129ebbde3c425a978436ce15f70d28
korinious <58111182+korinious@users.noreply.github.com>
2023-01-05 17:21:07 +0200

    updated movie.py file with user input

--- movie.py
+++ movie.py
@@ -20,24 +20,24 @@ def main():
 
     def get_year(movie_tag):
         moviesplit = movie_tag.text.split()
-        year = moviesplit[-1] # last item 
+        year = moviesplit[-1]
         return year
 
     years = [get_year(tag) for tag in movietags]
-    actors_list =[tag['title'] for tag in inner_movietags] # access attribute 'title'
+    actors_list =[tag['title'] for tag in inner_movietags]
     titles = [tag.text for tag in inner_movietags]
-    ratings = [float(tag['data-value']) for tag in ratingtags] # access attribute 'data-value'
+    ratings = [float(tag['data-value']) for tag in ratingtags]
 
     n_movies = len(titles)
 
-    #while(True):
+while(True):
     idx = random.randrange(0, n_movies)
         
     print(f'{titles[idx]} {years[idx]}, Rating: {ratings[idx]:.1f}, Starring: {actors_list[idx]}')
 
-        #user_input = input('Do you want another movie (y/[n])? ')
-        #if user_input != 'y':
-        #     break
+    user_input = input('Do you want another movie (y/[n])? ')
+    if user_input != 'y':
+        break
     
 
 if __name__ == '__main__':
