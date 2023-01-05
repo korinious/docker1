bbbd44aa106fa5be3547430af3caec7be7bb3223
korinious <korinakourkouta@gmail.com>
2023-01-05 16:41:01 +0200

    updated url in py file

--- movie.py
+++ movie.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 # crawl IMDB Top 250 and randomly select a movie
 
-URL = 'http://www.imdb.com/chart/top'
+URL = 'https://imdb.com/chart/top'
 
 def main():
     response = requests.get(URL)
